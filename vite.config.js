11331be17870796e93912c00d2711943f341384a
dilaouid <dilaouid@student.42.fr>
2025-05-17 15:09:51 +0200

    deploy(demo): deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
+const base = process.env.NODE_ENV === 'production' 
+  ? '/vaultic-crypto-engine-demo/'
+  : '';
+
 export default defineConfig({
   plugins: [
     wasm(),
@@ -9,5 +13,6 @@ export default defineConfig({
   ],
   optimizeDeps: {
     exclude: ['vaultic-crypto-engine']
-  }
+  },
+  base: base
 });
